f92a4bcd2db8a3ce3e8626fe535ea6358498ec6a
Janne Laukkanen <janne.laukkanen@helsinki.fi>
2011-09-18 16:49:56 +0300

    project startup

---
+++ mytab/mytab/main/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class Appartment(models.Model):
+    address = models.CharField(max_length=40)
+    habitants = models.IntegerField()
+
+    def __unicode__(self):
+        return self.address
+
+class Habitant(models.Model):
+    name = models.CharField(max_length=40)
+    address = models.ForeignKey(Appartment)
+
+    def __unicode__(self):
+        return self.name
