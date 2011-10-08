29167114bed1b08cc5540100504fd869bbf8eb45
Janne Laukkanen <janne.laukkanen@helsinki.fi>
2011-10-08 23:14:14 +0300

    created all initial models

--- mytab/mytab/main/models.py
+++ mytab/mytab/main/models.py
@@ -2,22 +2,77 @@ from django.db import models
 
 # Create your models here.
 
+class House(models.Model):
+    road = models.CharField(max_length=40)
+    number = models.CharField(max_length=5)
+    real_estate_agency = models.CharField(max_length=40, blank=True)
+
+    def __unicode__(self):
+        return self.road + " " + self.number 
+    
 class Appartment(models.Model):
-    address = models.CharField(max_length=40)
-    habitants = models.IntegerField()
-
-    def __unicode__(self):
-        return self.address
-
-class Habitant(models.Model):
-    name = models.CharField(max_length=40)
-    address = models.ForeignKey(Appartment)
-
-    def __unicode__(self):
-        return self.number
-
-class Key(models.Model):
-    number = models.IntegerField()
+    road = models.ForeignKey(House, to_field=House.road, unique_together=True)
+    road_number = models.ForeignKey(House, to_field=House.number, unique_together=True)
+    stair = models.CharField(max_length=2, unique_together=True)
+    appartment_number = models.IntegerField(unique_together=True)
         
     def __unicode__(self):
-        return self.number
+        return self.road + " " + self.road_number + " " + self.stair + " " + self.appartment_number
+
+class Habitant(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    social_security = models.CharField(11)
+    house = models.ForeignKey(House, blank=True)
+    appartment = models.ForeignKey(Appartment, blank=True)
+    phone = models.IntegerField(blank=True)
+
+    def __unicode__(self):
+        return self.first_name + " " + self.last_name
+
+class Key(models.Model):
+    key_id = models.IntegerField(primary_key=True)
+    appartment = models.ForeignKey(Appartment)
+    owner = models.ForeignKey(Habitant, blank=True)
+    key_acquired_date = models.DateField()
+    key_given_back_date = models.DateField()
+    
+    def __unicode__(self):
+        return self.key_id
+    
+class TargetOfReservation(models.Model):
+    house = models.ForeignKey(House)
+    name = models.CharField(30)
+    info = models.TextField()
+    
+    def __unicode__(self):
+        return self.house + "/" + self.name
+    
+class Reservation(models.Model):
+    number = models.IntegerField(primary_key=True)
+    maker = models.ForeignKey(Habitant)
+    created_date = models.DateTimeField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    
+    def __unicode__(self):
+        return self.number + " created:" + self.created_date
+
+class Employee(models.Model):
+    name = models.CharField(max_length=40)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField(blank=True)
+    
+    def __unicode__(self):
+        return self.name
+
+class WorkOrder(models.Model):
+    order_creator = models.ForeignKey(Employee)
+    order_finisher = models.ForeignKey(Employee, blank=True)
+    target = models.CharField(max_length=40)
+    info = models.TextField()
+    order_made_date = models.DateTimeField()
+    work_done_date = models.DateTimeField()
+    
+    def __unicode__(self):
+        return self.pk
--- mytab/mytab/main/tests.py
+++ mytab/mytab/main/tests.py
@@ -5,10 +5,31 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
-from django.test import TestCase
+from mytab.main.models import *
+import unittest
 
-
-class SimpleTest(TestCase):
+class Test_initialTests(unittest.TestCase):
+    
+    @classmethod
+    def setUpClass(cls):
+        
+        House.objects.all.delete()
+        Appartment.objects.all.delete()
+        Habitant.objects.all.delete()
+        
+        House.objects.create(address="Kuohikatu 5", real_estate_agent='Vasara', number_of_appartments=30, number_of_stairs=3)
+        House.objects.create(address="Kuohikatu 10", real_estate_agent='Vasara', number_of_appartments=20, number_of_stairs=2)
+        
+        Appartment.objects.create(house="Kuohikatu 5", stair="A", number=1, number_of_habitants=3, number_of_keys=4)
+        Appartment.objects.create(house="Kuohikatu 5", stair="A", number=2, number_of_habitants=2, number_of_keys=3)
+        Appartment.objects.create(house="Kuohikatu 5", stair="A", number=3, number_of_habitants=1, number_of_keys=3)
+        
+        Habitant.objects.create(first_name="Erkki", last_name="Latvala", ss="123456-1234", house="Kuohikatu 5", appartment="")
+        Habitant.objects.create(first_name="Matti", last_name="Latvala", ss="123456-5422", house="Kuohikatu 5", appartment="")
+        Habitant.objects.create(first_name="Pekka", last_name="Latvala", ss="010196-845J", house="Kuohikatu 5", appartment="")
+        
+        Habitant.objects.create(first_name="Erkki", last_name="Latvala", ss="150353-1234")
+        
     def test_basic_addition(self):
         """
         Tests that 1 + 1 always equals 2.
